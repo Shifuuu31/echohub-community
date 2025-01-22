bedcea8d80525c96936fb5a8fca154c5d855cc34
zziadcho <ziad.choukrii@gmail.com>
2025-01-22 19:42:15 +0100

    +switched from classes to ids +fixed popup exit when the whole post is clicked +fixed post-creation limit +responsive modifications

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,19 @@
 export { Popup }
 
-const postContent = document.querySelector("#post .post-body")
-const commentButton = document.querySelector("#post .post-categories button:nth-child(3)")
-const popupBackground = document.querySelector(".popup-background")
+const postContent = document.querySelector("#post")
+const commentButton = document.querySelector("#post #post-categories button:nth-child(3)")
+const popupBackground = document.querySelector("#popup-background")
 const closeButton = document.querySelector("#user-info-and-buttons button")
 
 
 const OpenPopup = () => {
-    document.querySelector(".popup-background").style.display = "block"
+    document.querySelector("#popup-background").style.display = "block"
     document.getElementById("popup").style.display = "block"
     document.body.style.overflow = "hidden"
 }
 
 const ClosePopup = () => {
-    document.querySelector(".popup-background").style.display = "none"
+    document.querySelector("#popup-background").style.display = "none"
     document.getElementById("popup").style.display = "none"
     document.body.style.overflow = "auto"
 }
