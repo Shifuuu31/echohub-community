97d74f0dc272e2cca1dd2fde6e6bb7c6c1d50ae0
zziadcho <ziad.choukrii@gmail.com>
2025-01-22 12:12:13 +0100

    +class/id name update +code cleanup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,19 @@
 export { Popup }
 
-const postContent = document.querySelector("#post .post-info-2")
-const commentButton = document.querySelector("#post .post-info-3 button:nth-child(3)")
-const popupBackground = document.querySelector(".popup-backgroud")
-const closeButton = document.querySelector("#popup-comment button")
+const postContent = document.querySelector("#post .post-body")
+const commentButton = document.querySelector("#post .post-categories button:nth-child(3)")
+const popupBackground = document.querySelector(".popup-background")
+const closeButton = document.querySelector("#user-info-and-buttons button")
 
 
 const OpenPopup = () => {
-    document.querySelector(".popup-backgroud").style.display = "block"
+    document.querySelector(".popup-background").style.display = "block"
     document.getElementById("popup").style.display = "block"
     document.body.style.overflow = "hidden"
 }
 
 const ClosePopup = () => {
-    document.querySelector(".popup-backgroud").style.display = "none"
+    document.querySelector(".popup-background").style.display = "none"
     document.getElementById("popup").style.display = "none"
     document.body.style.overflow = "auto"
 }
