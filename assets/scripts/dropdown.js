1bd5c04e80f9c2d1a953a2e811ede398e1fb11ef
asaadane <ayman.saadane@gmail.com>
2025-01-27 01:35:19 +0100

    enhance dropdown logic

--- assets/scripts/dropdown.js
+++ assets/scripts/dropdown.js
@@ -1,31 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    DropDown()  
+    DropDown()
 })
 
 const DropDown = () => {
     const profilePictureContainer = document.getElementById('profile-picture-container')
     const dropdown = document.getElementById('dropdown')
-    if (profilePictureContainer && dropdown) {
-        let isDropdownOpen = false
 
-        const toggleDropdown = (event) => {
-            event.stopPropagation()
-            isDropdownOpen = !isDropdownOpen
-            dropdown.classList.toggle('active')
-        }
-
-        const closeDropdown = (event) => {
-            if (isDropdownOpen && !dropdown.contains(event.target) && !profilePictureContainer.contains(event.target)) {
-                isDropdownOpen = false
-                dropdown.classList.remove('active')
-            }
-        }
-
-        profilePictureContainer.addEventListener('click', toggleDropdown)
-        document.addEventListener('click', closeDropdown)
-
-        dropdown.addEventListener('click', (event) => {
-            event.stopPropagation()
-        })
+    const toggleDropdown = (event) => {
+        event.stopPropagation()
+        dropdown.classList.toggle('active')
     }
+
+    profilePictureContainer.addEventListener('click', toggleDropdown)
+    document.addEventListener('click', ()=>{dropdown.classList.remove('active')})
 }
