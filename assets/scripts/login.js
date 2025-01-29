007e1bbc3463d5a0d203b3bff3a4f6abeccedd6c
asaadane <ayman.saadane@gmail.com>
2025-01-29 02:01:15 +0100

    easy feature kanmout 3lihom ana

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -2,14 +2,25 @@ import { fetchResponse, displayMessages } from "./tools.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('loginBtn')
+    const passwordInput = document.getElementById("password")
+    const passShow = document.getElementById('passShow')
 
+    passShow.addEventListener('click', () => {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text"
+            passShow.src = '/assets/imgs/visible.png'
+        }else {
+            passwordInput.type = "password"
+            passShow.src = '/assets/imgs/unvisible.png'
+        }
+    })
+    
     loginBtn.addEventListener('click', async (event) => {
-        console.log('dsds')
         event.preventDefault()
 
         const credentials = {
             username: document.getElementById('username').value,
-            password: document.getElementById('password').value,
+            password: passwordInput.value,
             rememberMe: document.getElementById('remember').checked,
         }
 
--- assets/scripts/register.js
+++ assets/scripts/register.js
@@ -3,6 +3,29 @@ import { fetchResponse, displayMessages } from "./tools.js"
 
 document.addEventListener('DOMContentLoaded', () => {
     const registerBtn = document.getElementById('registerBtn')
+    const passwordInput = document.getElementById("password")
+    const rPasswordInput = document.getElementById("rPassword")
+    const passShow = document.getElementById('passShow')
+    const rPassShow = document.getElementById('rPassShow')
+
+    passShow.addEventListener('click', () => {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text"
+            passShow.src = '/assets/imgs/visible.png'
+        }else {
+            passwordInput.type = "password"
+            passShow.src = '/assets/imgs/unvisible.png'
+        }
+    })
+    rPassShow.addEventListener('click', () => {
+        if (rPasswordInput.type === "password") {
+            rPasswordInput.type = "text"
+            rPassShow.src = '/assets/imgs/visible.png'
+        }else {
+            rPasswordInput.type = "password"
+            rPassShow.src = '/assets/imgs/unvisible.png'
+        }
+    })
 
     registerBtn.addEventListener('click', async (event) => {
         event.preventDefault()
@@ -10,8 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const newUser = {
             username: document.getElementById('username').value,
             email: document.getElementById('email').value,
-            password: document.getElementById('password').value,
-            rpassword: document.getElementById('rPassword').value,
+            password: passwordInput.value,
+            rpassword: rPasswordInput.value,
         }
 
         try {
