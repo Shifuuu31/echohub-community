4a14573a2fe5b8a7dccd85f607631e2146fb3c24
mrhioui <mohamedrhioui82@gmail.com>
2025-01-30 14:53:19 +0100

    refix post in fetchdata.js

--- assets/scripts/fetch-posts.js
+++ assets/scripts/fetch-posts.js
@@ -32,31 +32,30 @@ const desplayPosts = async (category = "All", append = false) => {
         if (FetchedPosts) {
             for (let i = 0; i < FetchedPosts.length; i++) {
                 const postData = document.createElement('div')
-                postData.innerHTML = `
-                <div id="post">
-                <div id="user-post-info"><img src="/assets/imgs/avatar.png" alt="User Avatar" loading="lazy">
-                <h3>@${FetchedPosts[i].PostUserName} <br><span>${new Date(FetchedPosts[i].PostTime).toUTCString()}</span></h3>
-                <div id="dropdown-content" style="margin-left:auto">
-                <a href="/updatePost?ID=${FetchedPosts[i].PostId}" target="_blank "><img src="/assets/imgs/update.png" style="border:none; border-radius:0px;"> Update Post</a>
-                <hr>
-                <a href="/deletePost?ID=${FetchedPosts[i].PostId}"><img src="/assets/imgs/delete.png" style="border:none; border-radius:0px;"> Delete Post</a>
-                </div>
-                </div>
-                <div id="post-body">
-                <h3>${FetchedPosts[i].PostTitle}</h3>
-                <pre>${wrapLinks(FetchedPosts[i].PostContent)}</pre>
-                </div>
-                <div id="post-categories">
-                <div id="links">
-                ${(FetchedPosts[i].PostCategories || []).map(category => `<li>${category}</li>`).join(' ')}
-                </div>
-                <div id="buttons">
-                <button><img src="/assets/imgs/like.png" alt="Like"> ${FetchedPosts[i].LikeCount}</button>
-                <button><img src="/assets/imgs/dislike.png" alt="Dislike"> ${FetchedPosts[i].DislikeCount}</button>
-                <button id="commentBtn"><img src="/assets/imgs/comment.png" alt="Comment"> ${FetchedPosts[i].CommentsCount}</button>
-                </div>
-                </div>
-                </div>`
+                postData.innerHTML = `          
+                <div id="post" post-id="${FetchedPosts[i].PostId}">
+                    <div id="user-post-info"><img src="/assets/imgs/avatar.png" alt="User Avatar" loading="lazy">
+                        <h3>@${FetchedPosts[i].PostUserName} <br><span>${new Date(FetchedPosts[i].PostTime).toUTCString()}</span></h3>
+                        <div id="dropdown-content" style="margin-left:auto">
+                            <a href="/updatePost?ID=${FetchedPosts[i].PostId}"><img src="/assets/imgs/update.png" style="border:none; border-radius:0px;"> Update Post</a>
+                            <hr>
+                            <a href="/deletePost?ID=${FetchedPosts[i].PostId}"><img src="/assets/imgs/delete.png" style="border:none; border-radius:0px;"> Delete Post</a>
+                        </div>
+                    </div>
+                    <div id="post-body">
+                        <h3 id="post-title">${FetchedPosts[i].PostTitle}</h3>
+                        <pre>${wrapLinks(FetchedPosts[i].PostContent)}</pre>
+                    </div>
+                    <div id="post-categories">
+                        <div id="links">
+                            ${(FetchedPosts[i].PostCategories || []).map(category => `<li>${category}</li>`).join(' ')}
+                        </div>
+                        <div id="buttons" >
+                            <button><img src="/assets/imgs/like.png" alt="Like"> ${FetchedPosts[i].LikeCount}</button>
+                            <button><img src="/assets/imgs/dislike.png" alt="Dislike"> ${FetchedPosts[i].DislikeCount}</button>
+                            <button id="commentBtn"><img src="/assets/imgs/comment.png" alt="Comment"> ${FetchedPosts[i].CommentsCount}</button>
+                        </div>
+                    </div>`
                 posts.append(postData)
             }
             DataToFetch.postID = FetchedPosts[FetchedPosts.length - 1].PostId
